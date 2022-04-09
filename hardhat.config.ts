1cdbc2728fa576a817b2514e6716b8b49ded2854
Asa Oines <asaoines@Asas-MacBook-Pro.local>
2022-04-09 09:17:55 -0400

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,26 @@
+import '@abacus-network/hardhat';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.13',
+      },
+    ],
+  },
+  gasReporter: {
+    currency: 'USD',
+  },
+  typechain: {
+    outDir: './types',
+    target: 'ethers-v5',
+    alwaysGenerateOverloads: false,
+  },
+};
