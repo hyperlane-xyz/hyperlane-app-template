ae53dd8d18eff8103f388637d2e8f62f0a03dd58
Asa Oines <asaoines@Asas-MacBook-Pro.local>
2022-04-10 21:46:46 -0400

    Add response tests

--- contracts/PingPong.sol
+++ contracts/PingPong.sol
@@ -17,6 +17,8 @@ The contracts keep track of the number of volleys in a given match,
 and emit events for each Sent and Received volley so that spectators can watch.
 */
 contract PingPong is Router {
+    uint256 public sent;
+    uint256 public received;
 
     // ============ Events ============
     event Sent(
@@ -31,54 +33,33 @@ contract PingPong is Router {
         bool isPing
     );
 
-    modifier onlySelf() {
-      require(msg.sender == address(this), "!self");
-        _;
-    }
-
     // ============ Constructor ============
     constructor(address _connectionManager) {
-      // TODO: Move to Router
-        __XAppConnectionClient_initialize(_connectionManager);
+        __Router_initialize(_connectionManager);
     }
 
-    function handlePing(uint32 _origin) external onlySelf {
-      emit Received(_localDomain(), _origin, true);
-      _sendPong(_origin);
-    }
-
-    function handlePong(uint32 _origin) external onlySelf {
-      emit Received(_localDomain(), _origin, false);
-      _sendPing(_origin);
-    }
-
-    function pingRemote(uint32 _destination) external onlySelf {
-        _sendPing(_destination);
-    }
-
-    function _sendPing(uint32 _destination) internal {
+    function pingRemote(uint32 _destination) external {
       _send(_destination, true);
-    }
-
-    function _sendPong(uint32 _destination) internal {
-      _send(_destination, false);
-    }
-
-    function _send(uint32 _destination, bool _isPing) internal {
-      uint32 localDomain = _localDomain();
-      _dispatchToRemoteRouter(
-        _destination,
-        abi.encodeCall(
-          _isPing ? PingPong.handlePong : PingPong.handlePing,
-          (localDomain)
-        )
-      );
-      emit Sent(localDomain, _destination, _isPing);
     }
 
     function _handle(
         bytes calldata _message
     ) internal override returns (bool, bytes memory) { 
-        return address(this).call(_message);
+        received += 1;
+        (uint32 _origin, bool _isPing) = abi.decode(_message, (uint32, bool));
+        emit Received(_origin, _localDomain(), _isPing);
+        _send(_origin, !_isPing);
+        return (true, "");
     }
-}
+
+    function _send(uint32 _destination, bool _isPing) internal {
+      address recipient = address(uint160(uint256(_mustHaveRemoteRouter(_destination))));
+      sent += 1;
+      uint32 localDomain = _localDomain();
+      _dispatchToRemoteRouter(
+        _destination,
+        abi.encode(localDomain, _isPing)
+      );
+      emit Sent(localDomain, _destination, _isPing);
+    }
+  }
