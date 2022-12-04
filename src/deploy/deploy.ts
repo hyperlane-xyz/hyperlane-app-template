a772d91a823fe83cda524181b73b74e8063ce5c4
J M Rossy <jm.rossy@gmail.com>
2022-12-04 11:46:42 -0500

    Sync with v1 branch instead of main

--- src/deploy/deploy.ts
+++ src/deploy/deploy.ts
@@ -34,7 +34,7 @@ export class HelloWorldDeployer<
   // If no custom logic is needed, call deployContract for the router
   async deployContracts(chain: Chain, config: HelloWorldConfig) {
     const router = await this.deployContract(chain, 'router', [
-      config.mailbox,
+      config.connectionManager,
       config.interchainGasPaymaster,
     ]);
     return {
